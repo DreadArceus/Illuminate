c6b56ac668b3ce9fbb9c0ee67bfffaef69b44ffa
SouhardyaMalakar <71064829+SouhardyaMalakar@users.noreply.github.com>
2021-03-07 19:02:25 +0530

    pepew (#1)

---
+++ my-app/src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import '../../App.css';
+import Left from '../Left';
+import Middle from '../Middle.js'
+import Cards from '../Cards'
+
+function Home() {
+    return(
+        <div class='main'>
+         <Left />
+         <Middle />
+         </div>
+         
+        
+        
+    )
+
+}
+export default Home;
