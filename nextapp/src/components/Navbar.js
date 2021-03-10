28b19f7c5ac6ba996b7e9297511f7ee196a39d2c
SouhardyaMalakar <71064829+SouhardyaMalakar@users.noreply.github.com>
2021-03-10 18:18:07 +0530

    pepew (#2)

--- nextapp/src/components/Navbar.js
+++ nextapp/src/components/Navbar.js
@@ -2,30 +2,17 @@ import React, { useEffect, useState } from "react";
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+ 
 
   const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
-
-  useEffect(() => {
-    showButton();
-  }, []);
-
-  // window.addEventListener('resize', showButton)
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          Menu
+          <div className="navbar-logo">
+            Menu 
+            </div>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
