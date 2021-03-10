403c1d271eb67cf209e56acd072ae78bf844e8aa
SouhardyaMalakar <souhardyamalakar.cob@gmail.com>
2021-03-10 17:17:44 +0530

    pepew

--- nextapp/src/pages/sample.tsx
+++ nextapp/src/pages/sample.tsx
@@ -2,14 +2,17 @@ import Left from "../components/Left";
 import Middle from "../components/Middle";
 import Navbar from "../components/Navbar";
 
+
 function Home() {
   return (
+    <div className="pepew">
     <div className="container">
       <Navbar />
       <div className="main">
         <Left />
         <Middle />
       </div>
+    </div>
     </div>
   );
 }
